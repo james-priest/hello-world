530a499b64037093641030ac04fec4bf4d0fd588
James Priest Dev <jpriest.velodea@gmail.com>
2017-05-31 16:25:17 -0700

    Add script.js file and created js folder

---
+++ js/script.js
@@ -0,0 +1,15 @@
+var Programmer = {
+    name: "James Priest",
+    email: "james-priest@outlook.com",
+    // projects: [{
+    //     project: "proj1",
+    //     desc: "desc1" },
+    //     {project: "proj2",
+    //     desc: "desc2"}]
+    DOB: "7/16/1970",
+    languages: ['HTML5', 'CSS3', 'JS'],
+
+    currentClass = function() {
+        console.log("I'm learning GitHub's Fork & Pull Request Workflow")
+    }
+}
